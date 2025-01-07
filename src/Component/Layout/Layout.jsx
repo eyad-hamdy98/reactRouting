c1344b23b14593a3370092a9b1a216f6fe641546
Eyad-Hamdy <eyad.hamdy98@gmail.com>
2025-01-07 18:59:37 +0200

    first commit

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Navbar from './../Navbar/Navbar';
-import Footer from './../Footer.jsx/Footer';
 import { Outlet } from 'react-router-dom';
+import Footer from './../Footer/Footer';
+
 
 export default function Layout() {
   return <>
